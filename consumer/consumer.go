418d0ec6ceced64e19e30fe7d0d7d854541941ed
codeabuu <abdulngui718@gmail.com>
2024-07-10 18:09:50 -0400

    consuming messages from kafka

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -15,31 +15,48 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	consumer.ConsumerPartition(topic, 0, sarama.OffsetOldest)
+
+	partitionConsumer, err := consumer.ConsumePartition(topic, 0, sarama.OffsetOldest)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("consumer started")
+	defer partitionConsumer.Close()
+
+	fmt.Println("Consumer started")
 	sigchan := make(chan os.Signal, 1)
 	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
 
 	msgCount := 0
-
 	doneCh := make(chan struct{})
 
-	go func ()  {
+	go func() {
 		for {
 			select {
-			case err := <-consumer.Error():
+			case err := <-partitionConsumer.Errors():
 				fmt.Println(err)
-			case msg := <-consumer.Messages():
+			case msg := <-partitionConsumer.Messages():
 				msgCount++
-				fmt.Printf("Received message count: %d: | Topic (%s) | Message(%s)n", msgCount, string(msg.Topic), string(msg.Value))
+				fmt.Printf("Received message count: %d: | Topic (%s) | Message(%s)\n", msgCount, string(msg.Topic), string(msg.Value))
 			case <-sigchan:
-				fmt.println("Interuption detected")
-				doneCh <- struct{{}}
-			
+				fmt.Println("Interruption detected")
+				doneCh <- struct{}{}
 			}
 		}
 	}()
+
+	<-doneCh
+	fmt.Println("Processed", msgCount, "messages")
+	if err := consumer.Close(); err != nil {
+		panic(err)
+	}
+}
+
+func connectConsumer(brokersUrl []string) (sarama.Consumer, error) {
+	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true
+	conn, err := sarama.NewConsumer(brokersUrl, config)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
 }
